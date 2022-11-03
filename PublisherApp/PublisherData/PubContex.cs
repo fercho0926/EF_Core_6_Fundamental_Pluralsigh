dd53376a3340a338a1965a34ae0e6049e7cabeef
Milton Vasquez <milton.vasquez@kineticadvantage.com>
2022-11-03 11:44:56 -0500

    lets beging

---
+++ PublisherApp/PublisherData/PubContex.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublisherDomain;
+
+namespace PublisherData
+{
+    public class PubContex : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=EN2010480;Database=EF_Core_6_Fundamental_Pluralsigh;Trusted_Connection=True;MultipleActiveResultSets=true" );
+        }
+    }
+}
